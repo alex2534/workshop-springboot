fed0d114d967856c11149314246c9e7e8e216feb
Alex <alex2534@outlook.com>
2023-02-03 14:59:41 -0300

    H2 database, test profile, JPA

--- src/main/java/com/estudando/course/entites/User.java
+++ src/main/java/com/estudando/course/entites/User.java
@@ -2,6 +2,11 @@ package com.estudando.course.entites;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,10 +20,14 @@ import lombok.Setter;
 @AllArgsConstructor
 @Data
 @EqualsAndHashCode
+@Entity //para o jpa converter o BD para o modelo relacional
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//isso e usado para alto encremento
 	private Long id;
 	@EqualsAndHashCode.Exclude private String name;
 	@EqualsAndHashCode.Exclude private String email;
